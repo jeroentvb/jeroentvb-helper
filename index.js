2c5f702fd4e8820ce53cc2fc4199821cb01798f6
jeroentvb <jeroen.van.berkum@hva.nl>
2018-11-28 15:27:08 +0100

    Don't stringify if already a string instead of throwing an error

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ class Helper {
 
   static exportToFile (name, item) {
     if (name === undefined || item === undefined) throw new Error('Error while exporting: parameters not defined')
+    if (typeof item !== 'string') item = Helper.stringify(item)
     var fs = require('fs')
-    fs.writeFile(name + '-export.json', Helper.stringify(item), err => {
+    fs.writeFile(name + '-export.json', item, err => {
       if (err) throw err
       console.log(`${name}-export.json written`)
     })
