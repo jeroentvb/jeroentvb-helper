264016a72a8774165ba17e84f74065b25cff893f
jeroentvb <jeroentvb@hotmail.com>
2019-03-01 16:57:11 +0100

    Removed use of a class

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-class Helper {
-  static stringify (item) {
-    if (typeof item === 'string') throw new Error(`Error while stringifying: ${item} is already a string`)
-    return JSON.stringify(item, null, 4)
-  }
-
-  static exportToFile (name, item) {
-    if (name === undefined || item === undefined) throw new Error('Error while exporting: parameters not defined')
-    if (typeof item !== 'string') item = Helper.stringify(item)
-    var fs = require('fs')
-    fs.writeFile(name + '-export.json', item, err => {
-      if (err) throw err
-      console.log(`${name}-export.json written`)
-    })
-  }
+function stringify (item) {
+  if (typeof item === 'string') throw new Error(`Error while stringifying: ${item} is already a string`)
+  return JSON.stringify(item, null, 4)
 }
 
-module.exports = Helper
+function exportToFile (name, item) {
+  if (name === undefined || item === undefined) throw new Error('Error while exporting: parameters not defined')
+  if (typeof item !== 'string') item = stringify(item)
+  const fs = require('fs')
+  fs.writeFile(name + '-export.json', item, err => {
+    if (err) throw err
+    console.log(`${name}-export.json written`)
+  })
+}
+
+module.exports = {
+  stringify,
+  exportToFile
+}
